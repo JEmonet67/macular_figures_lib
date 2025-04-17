f99b52648f49fb14d31a682943639afecdc5b202
Emonet <jerome.emonet.67@gmail.com>
2025-04-17 17:22:25 +0200

    Implementation of conversion functions between numpy and Macular coordinates

--- src/data_manager/CoordinateManager.py
+++ src/data_manager/CoordinateManager.py
@@ -72,3 +72,59 @@ class CoordinateManager:
             for x_col in range(0, last_cell)]
 
         return list_time_bar_center
+
+    @staticmethod
+    def convert_coord_macular_to_coord_numpy(dict_coord_macular, n_cells):
+        """Function to transform the coordinate of a cell in the Macular coordinate system to that of Numpy.
+
+        The Macular coordinate system differs from the Numpy coordinate system by a 90° clockwise rotation.
+
+        Parameters
+        ----------
+        dict_coord_macular : dict
+            Dictionary containing the Macular coordinates to be transformed. The dictionary contains an ‘x’ and a ‘y’
+            key.
+
+        n_cells : tuple
+            Size of the Macular graph in cells in the form of : (number of cells in x, number of cells in y).
+
+        Returns
+        ----------
+        dict_coord_numpy : dict
+            Dictionary containing the Numpy coordinates of the cell whose Macular coordinates have been provided.
+            The dictionary contains an ‘x’ and a ‘y’ key.
+        """
+        dict_coord_numpy = {}
+
+        dict_coord_numpy["x"] = (n_cells[1] - 1) - dict_coord_macular[1]
+        dict_coord_numpy["y"] = dict_coord_macular[0]
+
+        return dict_coord_numpy
+
+    @staticmethod
+    def convert_coord_numpy_to_coord_macular(dict_coord_numpy, n_cells):
+        """Function to transform the coordinate of a cell in the Numpy coordinate system to that of Coordinates.
+
+        The Numpy coordinate system differs from the Macular coordinate system by a 90° anti-clockwise rotation.
+
+        Parameters
+        ----------
+        dict_coord_numpy : dict
+            Dictionary containing the Numpy coordinates to be transformed. The dictionary contains an ‘x’ and a ‘y’
+            key.
+
+        n_cells : tuple
+            Size of the Macular graph in cells in the form of : (number of cells in x, number of cells in y).
+
+        Returns
+        ----------
+        dict_coord_macular : dict
+            Dictionary containing the Macular coordinates of the cell whose Numpy coordinates have been provided.
+            The dictionary contains an ‘x’ and a ‘y’ key.
+        """
+        dict_coord_macular = {}
+
+        dict_coord_macular["x"] = dict_coord_numpy[1]
+        dict_coord_macular["y"] = (n_cells[0] - 1) - dict_coord_numpy[0]
+
+        return dict_coord_macular
