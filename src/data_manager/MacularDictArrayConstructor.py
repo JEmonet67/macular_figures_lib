fe44b62214cac0ce47ef7ec1e4d5d6308c1d0d10
Emonet <jerome.emonet.67@gmail.com>
2025-04-17 17:30:33 +0200

    Substitution terme output par measurements dans MacularDictArrayConstructor

--- src/data_manager/MacularDictArrayConstructor.py
+++ src/data_manager/MacularDictArrayConstructor.py
@@ -88,58 +88,63 @@ MultiDataDictArray
 
         return dataframe
 
-    @staticmethod
-    def init_dict_output_celltype_array(list_output_celltype):
-        list_unique_output_celltype = list(set(list_output_celltype))
-        print(f"Output cell type : {list_unique_output_celltype}")
-        dict_output_celltype_array = {output_celltype: []
-                                      for output_celltype in list_unique_output_celltype}
+    def get_list_num_measurements(self, path_csv_file):
+        columns = pd.read_csv(path_csv_file, nrows=0).columns[1:]
+        list_measurements, list_num = [], []
 
-        return dict_output_celltype_array
+        # Go through column names of the Macular dataframe.
+        for column in columns:
+            # Extraction of outputs, numbers and cell type.
+            (output, num, celltype) = self.output_num_celltype_reg.findall(column)[0]
 
-    @staticmethod
-    def extend_dict_output_celltype_array(dict_output_celltype_array, x, y, z):
-        print(f"Initialisation of array of size {x}x{y}x{z}...", end="")
-        for output_celltype in dict_output_celltype_array:
-            dict_output_celltype_array[output_celltype] += [np.zeros([x, y, z])]
+            # Incrementation of the lists of number and measurements (output_celltype).
+            list_num += [int(num)]
+            list_measurements += [f"{output}_{celltype}"]
 
-        return dict_output_celltype_array
+        return list_num, list_measurements
 
     @staticmethod
-    def fill_dict_output_celltype_array_chunk(dataframe_chunk, dict_output_celltype_array, list_output_celltype,
+    def init_dict_measurements_array(list_measurements):
+
+        # Create the measurements list without duplicates
+        list_unique_measurements = list(set(list_measurements))
+        print(f"Measurements : {list_unique_measurements}")
+
+        # Initialisation of the empty list dictionary of measurements.
+        dict_measurements_array = {measurements: []
+                                      for measurements in list_unique_measurements}
+
+        return dict_measurements_array
+
+    @staticmethod
+    def extend_dict_measurements_array(dict_measurements_array, x, y, z):
+        print(f"Implementation of array of size {x}x{y}x{z}...", end="")
+        for measurements in dict_measurements_array:
+            dict_measurements_array[measurements] += [np.zeros([x, y, z])]
+
+        return dict_measurements_array
+
+    @staticmethod
+    def fill_dict_measurements_array_chunk(dataframe_chunk, dict_measurements_array, list_measurements,
                                               list_num, n_cells, i_chunk):
         print("Filling...", end="")
         for i, num in enumerate(list_num):
+            # Conversion of the Macular identification number into Macular coordinates
             macular_coord = CoordinateManager.id_to_coordinates(num, n_cells)
-            dict_output_celltype_array[list_output_celltype[i]][i_chunk][macular_coord["x"], macular_coord["y"]] = (
+            # Insert the 3D array of the given measurement and the given cell coordinates in the measurement dictionary.
+            dict_measurements_array[list_measurements[i]][i_chunk][macular_coord["x"], macular_coord["y"]] = (
                 dataframe_chunk.iloc[:, i].to_numpy())
 
-        return dict_output_celltype_array
+        return dict_measurements_array
 
     @staticmethod
-    def dict_output_celltype_array_rotation(dict_output_celltype_array, axes):
+    def dict_measurements_array_rotation(dict_measurements_array, axes):
         print("Rotating...", end="")
-        for key in dict_output_celltype_array:
-            dict_output_celltype_array[key] = np.rot90(dict_output_celltype_array[key], axes=axes)
+        for key in dict_measurements_array:
+            dict_measurements_array[key] = np.rot90(dict_measurements_array[key], axes=axes)
         print("Done!")
 
-        return dict_output_celltype_array
-
-    def name_extraction(self, path_data):
-        """Summary
-
-            Explanation
-
-            Parameters
-            ----------
-            param1 : type
-                Summary param1
-
-                Explanation param1
-            Returns
-            ----------
-            type
-                Summary
+        return dict_measurements_array
 
     def transient_extraction(self, path_csv_file):
         return int(self.transient_reg.findall(path_csv_file)[0][:-1])
@@ -155,15 +160,3 @@ MultiDataDictArray
         print("Done!")
 
         return dataframe
-
-    def get_list_num_output_celltype(self, path_csv_file):
-        columns = pd.read_csv(path_csv_file, nrows=0).columns[1:]
-        list_output_celltype, list_num = [], []
-
-        for column in columns:
-            (output, num, celltype) = self.output_num_celltype_reg.findall(column)[0]
-            list_num += [int(num)]
-            list_output_celltype += [f"{output}_{celltype}"]
-
-        return list_num, list_output_celltype
-
