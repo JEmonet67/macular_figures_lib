2cb83747bb5ada53b758cc1e9a3861a6c8943293
Emonet <jerome.emonet.67@gmail.com>
2025-04-17 17:24:52 +0200

    Premier jet de commentaires de DataPreprocessor et déplacement de print

--- src/data_manager/DataPreprocessor.py
+++ src/data_manager/DataPreprocessor.py
@@ -26,7 +26,6 @@ class DataPreprocessor:
 
     @staticmethod
     def vsdi_computing(macular_dict_array_data):
-        print("VSDI computing...", end="")
         # Set the average excitatory and inhibitory voltages.
         exc_mean_voltage = macular_dict_array_data["muVn_CorticalExcitatory"]
         inh_mean_voltage = macular_dict_array_data["muVn_CorticalInhibitory"]
@@ -53,8 +52,9 @@ class DataPreprocessor:
 
     @staticmethod
     def computing_binning_parameters(index, bin_time):
-        print(f"Binning {bin_time}s...", end="")
+        # Number of indices to be averaged within the binning time.
         bin_size = int(bin_time / round(index[1] - index[0], 6))
+        # Number of binning intervals to cover the index.
         n_bin = int((index[-1] - index[0]) / bin_time)
 
         return bin_size, n_bin
@@ -70,8 +70,9 @@ class DataPreprocessor:
 
     @staticmethod
     def temporal_centering(index, list_time_center):
-        print("Temporal centering...", end="")
+        # Incrementing the list of time indexes re-centred for each cell in the bar axis.
         list_index_centered = []
+
         for time_center in list_time_center:
             list_index_centered.append(index - time_center)
 
@@ -81,16 +82,21 @@ class DataPreprocessor:
     def derivative_computing_3d_array(array, index, n=1):
         """Function to compute derivative of a 3D array to be derived. The derivative can depend
         on n_value or to be instant."""
-        print("Derivating...", end="")
         df_dxdt = np.zeros(array.shape)
+
+        # Go through temporal index
         for i_derivate in range(array.shape[2]):
+            # Case where the time index is too close to the lower limit for the window to fit.
             if i_derivate < n:
                 df_dxdt[:, :, i_derivate] = (array[:, :, i_derivate + n] - array[:, :, 0])
                 df_dxdt[:, :, i_derivate] = df_dxdt[:, :, i_derivate] / (index[i_derivate + n] - index[0])
+
+            # Case where the time index is too close to the upper limit for the window to fit.
             elif i_derivate >= array.shape[2] - n:
                 df_dxdt[:, :, i_derivate] = (array[:, :, array.shape[2] - 1] - array[:, :, i_derivate - n])
                 df_dxdt[:, :, i_derivate] = df_dxdt[:, :, i_derivate] / (
                             index[array.shape[2] - 1] - index[i_derivate - n])
+            # Intermediate case where the n window fits within the neighbourhood of the current time index.
             else:
                 df_dxdt[:, :, i_derivate] = (array[:, :, i_derivate + n] - array[:, :, i_derivate - n])
                 df_dxdt[:, :, i_derivate] = df_dxdt[:, :, i_derivate] / (index[i_derivate + n] - index[i_derivate - n])
